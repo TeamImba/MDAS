153a62d9cada869d291d3c6d345cf7f513783afa
mrrussellong <mrrussellong@gmail.com>
2011-06-15 05:38:34 +0800

    fixing login

--- Gemfile
+++ Gemfile
@@ -1,12 +1,12 @@
 source 'http://rubygems.org'
 
 gem "rails", "3.0.7"
-gem "mysql"
+gem "ruby-mysql"
 gem "compass"
 gem "haml"
 gem "html5-boilerplate"
 gem "devise"
-gem "cancan"
+#gem "cancan"
 gem "rails_admin", :git => "git://github.com/sferik/rails_admin.git"
 gem "aws-s3", :require => "aws/s3"
 gem "paperclip", "~> 2.3"
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,8 @@ class ApplicationController < ActionController::Base
   before_filter :if => Proc.new{ |c| c.request.path =~ /admin/ } do 
     @head_stylesheet_paths = ['admin_screen.css'] 
   end
-  rescue_from CanCan::AccessDenied do |exception|
-    redirect_to root_url, :alert => exception.message
-  end
+#  rescue_from CanCan::AccessDenied do |exception|
+#    redirect_to root_url, :alert => exception.message
+#  end
   
 end
