d7251b400777b07c77a3fc3890277da13d5517c2
mrrussellong <mrrussellong@gmail.com>
2011-06-19 21:52:38 +0800

    signup section

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,8 +8,17 @@ class HomeController < ApplicationController
     @product = Product.find(params[:id])
     @product.view_count = @product.view_count.to_i + 1
     @product.save
-    
-    logger.info "errr"
     redirect_to @product.product_url
+  end
+  
+  def create
+    UserSignup.create(
+      :email => params[:email], 
+      :multiply_user_id => params[:muid],
+      :last_name => params[:ln],
+      :first_name => params[:fn],
+      :mobile_number => params[:mob]
+    )
+    render :nothing => true, :status => 200
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 NewRails::Application.routes.draw do
   devise_for :users
+  resource :home, :only => [:index, :show, :create], :controller => :home
   root :to => "home#index"
-  
   match 'product/:id' => "home#show", :as => "product"
 end
