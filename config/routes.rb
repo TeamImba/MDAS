aae25387473ed89c7af2cc56e5a91c155d87a733
mrrussellong <mrrussellong@gmail.com>
2011-07-04 06:45:37 +0800

    new implementations

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ NewRails::Application.routes.draw do
   resource :home, :only => [:index, :show, :create], :controller => :home
   root :to => "home#index"
   match 'product/:id' => "home#show", :as => "product"
+  match 'about' => "home#about", :as => "about"
+  match 'faqs' => "home#faqs", :as => "faq"  
 end
