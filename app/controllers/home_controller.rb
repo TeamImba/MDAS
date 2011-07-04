ecc14bfafc2616620c3eaaf9a02b63c06220a3f0
russell <mrrussellong@yahoo.com>
2011-07-04 10:41:49 +0800

    social fix

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -23,7 +23,8 @@ class HomeController < ApplicationController
   end
   
   def social
-    
+    #Cookie:  fbs_213790775324755="access_token=213790775324755%7C2.AQDPKUdO34D1xczG.3600.1309752000.1-783183828%7C7XhxJXdFMKGmoxygNn-AOm8CYbo&base_domain=www.multiplyphilippines.com&expires=1309752000&secret=PsnQ400mIzY21oXbit_j7g__&session_key=2.AQDPKUdO34D1xczG.3600.1309752000.1-783183828&sig=6fe2f3731a4bce084001637444eec602&uid=783183828";
+    #cookies[:fbs_213790775324755]
     render :nothing => true
   end
 end
